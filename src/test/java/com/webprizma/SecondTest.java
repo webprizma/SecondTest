aa3b3277885c7bc6e72ed738b3a867541dedec2e
Yuri Kulagin <kulagin1987@gmail.com>
2022-05-26 11:02:54 +0300

    first commit

---
+++ src/test/java/com/webprizma/SecondTest.java
@@ -0,0 +1,11 @@
+package com.webprizma;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+    @Test
+    void SecondTest() {
+        Assertions.assertTrue(3>2);
+    }
+}
